1d19782552869327a9538e905598ffed59d49672
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-04-24 19:54:00 -0300

    conseguimos filtrar nome-marca-tipo

--- main.js
+++ main.js
@@ -1,43 +1,38 @@
-
+let itens =[]
 const container = document.querySelector('.product-container');
-let nomev= ''
-let marcav= ''
-let tipov = ''
 const nome = document.getElementById('name');
 const marca = document.getElementById('marca');
 const tipo = document.getElementById('tipo');
 
-let endPointAPI = ` http://makeup-api.herokuapp.com/api/v1/products.json?brand=${marcav}&product_type=${tipov}&name=${nomev}`;
+let endPointAPI = ` http://makeup-api.herokuapp.com/api/v1/products.json?brand=${marca.value}&product_type=${tipo.value}`;
 
 
 getBuscaItensAPI(endPointAPI);
 
 async function getBuscaItensAPI(link){
     const resp = await fetch(link);
-    const itens = await resp.json(); 
+    itens = await resp.json(); 
     console.log(itens);
-    showItens(itens);
-    
+    showItens(itens);  
 }
 
-nome.addEventListener('input', () => {
-    nomev= nome.value
- getBuscaItensAPI(endPointAPI);
-} )
-
-marca.addEventListener('input', () => {
-    marcav= marca.value
+marca.addEventListener('blur', () => {
+    let endPointAPI = ` http://makeup-api.herokuapp.com/api/v1/products.json?brand=${marca.value}&product_type=${tipo.value}`;
     getBuscaItensAPI(endPointAPI);
    } )
 
-tipo.addEventListener('input', () => {
-    tipov= tipo.value
+tipo.addEventListener('blur', () => {
+    let endPointAPI = ` http://makeup-api.herokuapp.com/api/v1/products.json?brand=${marca.value}&product_type=${tipo.value}`;
     getBuscaItensAPI(endPointAPI);
    } )
 
-
+nome.addEventListener('blur',()=> {
+    itens = itens.filter(item => item.name.includes(nome.value))
+    showItens(itens);
+})
 
 function showItens(itens) {
+    container.innerHTML='';
     let display = itens.map(item =>{
         return (
             `<div class="product">
