35e4607132089f0aa10f2118a5256e6debf100c5
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-04-25 15:24:55 -0300

    finish

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const marca = document.getElementById('marca');
 const tipo = document.getElementById('tipo');
 const org = document.getElementById('org');
 
+const modalInfo = document.querySelector('.modalInfo');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('#close-modal');
+
 let endPointAPI = ` http://makeup-api.herokuapp.com/api/v1/products.json?brand=${marca.value}&product_type=${tipo.value}`;
 
 
@@ -53,20 +57,67 @@ org.addEventListener('input',()=> {
 
 function showItens(itens) {
     container.innerHTML='';
-    let display = itens.map(item =>{
+    let display = itens.map((item, index) =>{
         return (
-            `<div class="product">
-            <img class="product-figure" src=${item.image_link} alt="">
+            `<div class="product" data-index= ${index}>
+            <img class="product-figure" src=${item.api_featured_image} alt="">
             <section class="product-description">
                 <h1 class="product-name">${item.name}</h1>
                 <div class="product-brands">
                     <span class="brand">${item.brand}</span>
-                    <span class="price">${((+item.price)*5.5).toFixed(2)}</span>
+                    <span class="price"> R$ ${((+item.price)*5.5).toFixed(2)}</span>
                 </div>
             </section>
         </div>`
         )
     })
     display = display.join('')
-    container.innerHTML=display
+    container.innerHTML=display;
+
+
+
+    const productElements = document.querySelectorAll('.product');
+    productElements.forEach(e=> {
+        e.addEventListener('click', ()=> showInfo(e))
+    })
 }
+
+function showInfo(e) {
+    let itemElement = e.dataset.index;
+    console.log(itemElement);
+    modalInfo.innerHTML=`<div class="modalInfo_cabecalho">
+                            <h1>${itens[itemElement].name}</h1>
+                            <img src=${itens[itemElement].api_featured_image} alt="">
+                        </div>
+                        <div class="product-brands">
+                            <span class="brand">${itens[itemElement].brand}</span>
+                            <span class="price">${((+itens[itemElement].price)*5.5).toFixed(2)}</span>
+                        </div>
+                        <div class="product-brands-infos">
+                            <span>Brand</span>
+                            <span>${itens[itemElement].brand}</span>
+                        </div>
+                        <div class="product-brands-infos">
+                            <span>Price</span>
+                            <span>${((+itens[itemElement].price)*5.5).toFixed(2)}</span>
+                        </div>
+                        <div class="product-brands-infos">
+                            <span>Rating</span>
+                            <span>${itens[itemElement].rating}</span>
+                        </div>
+                        <div class="product-brands-infos">
+                            <span>Category</span>
+                            <span>${itens[itemElement].category}</span>
+                        </div>
+                        <div class="product-brands-infos">
+                            <span>Product_type</span>
+                            <span>${itens[itemElement].product_type}</span>
+                        </div>`
+    modal.showModal();
+}
+
+closeModal.addEventListener('click', () =>{
+    modal.close();
+    modalInfo.innerHTML='';
+    
+})
